00d2480c3ef3a832659d6313745337d28b37fafb
katyacher <katyacher@inbox.ru>
2024-12-12 23:13:32 +0300

    debugging io float numbers

--- src/lab2.s
+++ src/lab2.s
@@ -10,30 +10,31 @@
 #TODO реализовать очистку экрана clrscr()
 
 .file "lab2.s" # имя файла (необязательно)
-
+.global main
 .data
-    num0: .double 0.0
-    .balign 8
-    num2: .double 2.0 
-     .balign 8
-    num3: .double 3.0  
-     .balign 8
-    num5: .double 5.0
-     .balign 8
-    num7: .double 7.0
-   .balign 8
-    num9: .double 9.0
-     .balign 8
-    num15: .double 15.0
-     .balign 8
-     num1: .double 1.0
-	 .balign 8
+
     x: .double 0
-    .balign 8
+    .balign 16
     a: .double 0
-     .balign 8
+    .balign 16
     result: .double 0
- .balign 8
+    .balign 16
+    num0: .double 0.0
+    .balign 16
+    num2: .double 2.0 
+    .balign 16
+    num3: .double 3.0  
+    .balign 16
+    num5: .double 5.0
+    .balign 16
+    num7: .double 7.0
+    .balign 16
+    num9: .double 9.0
+    .balign 16
+    num15: .double 15.0
+    .balign 16
+    num1: .double 1.0
+    .balign 16
     msg: .ascii "Program calculates y = y1/y2\n"
     msg_arg_x: .ascii "Input number: x = "
     msg_arg_a: .ascii "Input number: a = "
@@ -41,35 +42,33 @@
     msg_res: .ascii "y = %lf\0 "
     s: .string "\n"
     .Lc0: .ascii "%lf\0" # форматная строка спецификатор .double с нулевым байтом 
-    .Lc1: .ascii "x = %lf\0" 
-    .Lc2: .ascii "a = %lf\0" 
+    .Lc1: .ascii "x = %lf \0" 
+    .Lc2: .ascii "a = %lf \0" 
     .Lc3: .ascii "%s"
 		
 .text
-.global main
+
 main:
-    #xorq %rdi,%rdi
-    #xorq %rsi,%rsi
     pushq %rbp 
     movq %rsp, %rbp 	# сохраняем начальное значение указателя стека в rbp
   
     # выведем строку msg на экран
     movq $1, %rax 		# sys_write
     movq $1, %rdi 		# стандартная консоль
-    movq $msg, %rsi 	# адрес начала строки
-    movq $29, %rdx 	# кол-во выводимых символов
+    movq $msg, %rsi 		# адрес начала строки
+    movq $29, %rdx 		# кол-во выводимых символов
     syscall
 	
     # выведем строку msg_arg_x на экран
     movq $1, %rax 		# write
     movq $1, %rdi 		# стандартная консоль
     movq $msg_arg_x, %rsi 	# адрес начала строки
-    movq $18, %rdx 	# кол-во выводимых символов
+    movq $18, %rdx 		# кол-во выводимых символов
     syscall
 		
    # pushq %rbp           	# keep stack aligned
     leaq .Lc0(%rip), %rdi 	# адрес начала форматной строки
-    leaq x(%rip), %rsi	# адрес сохранения введённой строки
+    leaq x(%rip), %rsi		# адрес сохранения введённой строки
     movq $1, %rax		# кол-во регистров xmmn
     callq scanf 		# вызов функции scanf
     #popq %rbp		
@@ -78,14 +77,14 @@ main:
     movq $1, %rax 		# write
     movq $1, %rdi 		# стандартная консоль
     movq $msg_arg_a, %rsi 	# адрес начала строки
-    movq $18, %rdx 	# кол-во выводимых символов
+    movq $18, %rdx 		# кол-во выводимых символов
     syscall
 		
    # pushq %rbp           	# keep stack aligned
     movq  $1, %rax       	# clear AL (zero FP args in XMM registers)
     leaq .Lc0(%rip), %rdi  	# адрес начала форматной строки
     leaq a(%rip), %rsi  	# адрес сохранения введённой строки
-    call scanf		# считаем число в переменную a
+    call scanf			# считаем число в переменную a
    # popq %rbp		
     
     # выполним вычисление y = y1/y2 при следующих параметрах:
@@ -107,7 +106,7 @@ main_loop:
     xorq %rax, %rax
     fstsw  %ax 
     sahf
-    jge arg2       	# если st(0) > st(1), 2 >= x или x<=2
+    jge arg2       	# если st(0) > st(1),  x<=2
     fabs		# st(0) = |x|
     fsubp %st(0), %st(1)		# st(0) = |x|-5 = y2 
     jmp y1		
@@ -127,15 +126,15 @@ main_loop:
     fstsw %ax 
     sahf
     jge arg1       	# если st(0) >= st(1), x<=7
-    fldl num15			# st(0) = 15.0, st(1) = x
-    faddp  %st(0), %st(1)	# st(0) = 15 + x = y1
-    jmp div1
-    arg1:
     fstp %st(0)	# удалить x
     fldl a
     fabs		#|a|
     fldl num9
     faddp %st(0), %st(1)	#|a| + 9 = st(0) = y1
+    jmp div1
+    arg1:
+    fldl num15			# st(0) = 15.0, st(1) = x
+    faddp  %st(0), %st(1)	# st(0) = 15 + x = y1
    
  				
     div1:
@@ -163,51 +162,34 @@ main_loop:
     # выведем результат на экран	
     res:
     # выведем на экран переменные
-    pushq %rbp
+   # pushq %rbp
    # movq %rsp, %rbp 	# сохраняем начальное значение указателя стека в rbp
-    pushq %rcx  	
+   # pushq %rcx  	
     xorq %rax, %rax
     movq $1, %rax              #1 регистр xmm0
     leaq .Lc1(%rip), %rdi 	# адрес начала форматной строки
     movq x(%rip), %rsi		#само выводимое значение x = 
     callq printf		#вызов функции printf
-    addq  $8, %rsp
-    popq  %rcx 
+    #addq  $8, %rsp
+   # popq  %rcx 
     
-    
-   # pushq %rcx  
-    #xorq %rax, %rax
-    #leaq .Lc3(%rip), %rdi 	# адрес начала форматной строки
-    #leaq s(%rip), %rsi		# само выводимое значение \n
-   # callq printf		# вызов функции printf
-   # addq  $8, %rsp
-    #popq  %rcx             #восстановить регистр             
- 
-           
-    pushq %rcx              # сохранить регистр
-    pushq $s  # напечатать символ новой строки     
-    call  printf
-    addq  $8, %rsp
-    popq  %rcx             #восстановить регистр             
+   # pushq %rcx              # сохранить регистр
+   # pushq $s  # напечатать символ новой строки     
+   # call  printf
+    #addq  $8, %rsp
+   # popq  %rcx             #восстановить регистр             
  m1:
-    pushq %rcx   
+   # pushq %rcx   
     xorq %rax, %rax
     leaq .Lc2(%rip), %rdi 	# адрес начала форматной строки
     movq a(%rip), %rsi	# само выводимое значение a = 
     callq printf		#вызов функции printf
-    addq  $16, %rsp          # выровнять стек  
-    popq  %rcx             #восстановить регистр             
+    #addq  $8, %rsp          # выровнять стек  
+    #popq  %rcx             #восстановить регистр             
  
-    pushq %rcx 
-    xorq %rax, %rax
-    leaq .Lc3(%rip), %rdi 	# адрес начала форматной строки
-    leaq s(%rip), %rsi		#само выводимое значение \n
-    callq printf		#вызов функции printf
-    addq  $8, %rsp
-    popq  %rcx 
-    
+   
     # выведем на экран результат
-    pushq %rbp
+   # pushq %rbp
     #movq %rsp, %rbp
     #subq $8, %rsp           # выравнивание должно быть по 16 байтам
     #pushq %rcx 
@@ -216,19 +198,19 @@ main_loop:
     movq $1, %rax 	    	 # кол-во регистров xmmn
     movq result(%rip), %rsi
     call printf
-    addq  $8, %rsp          # выровнять стек  
-    popq  %rcx 
+    #addq  $8, %rsp          # выровнять стек  
+    #popq  %rcx 
     
-    pushq %rcx 
+    #pushq %rcx 
     xorq %rax, %rax
     leaq .Lc3(%rip), %rdi 	# адрес начала форматной строки
     leaq s(%rip), %rsi		# само выводимое значение \n
     callq printf		# вызов функции printf
-    addq  $8, %rsp          # выровнять стек   
-    popq %rcx 
+   # addq  $8, %rsp          # выровнять стек   
+   # popq %rcx 
     
    # movq %rbp, %rsp 	# восстанавливаем стек
-    popq %rbp
+    #popq %rbp
     
 m:# для отладки  
    
